f81235571553ac12a629bd7816439ea8bcb60c9b
Isao Jonas <isao.jonas@gmail.com>
2013-01-31 09:22:19 -0600

    mo smarter builder support

--- test/statements.js
+++ test/statements.js
@@ -1,5 +1,5 @@
 var assert = require('assert'),
-	statement = require('../lib/statements');
+	query = require('../index');
 
 function eq(from, to)
 {
@@ -12,50 +12,21 @@ describe('statements', function()
 	{
 		it('should accept object', function()
 		{
-			eq(statement.CreateKeyspace(
-			{
-				keyspace: 'hello',
-				options: [
-					{key: 'replication', value: [{key: 'class', value: 'NetworkTopologyStrategy'}, {key: 'DC1', value: 1}, {key: 'DC2', value: 3}]}, 
-					{key: 'durable_writes', value: false}
-				]
-			}), 
+			eq(query.CreateKeyspace()
+				.keyspace('hello')
+				.options()
+					.value()
+						.key('replication')
+						.value([
+							{key: 'class', value: 'NetworkTopologyStrategy'},
+							{key: 'DC1', value: 1}, 
+							{key: 'DC2', value: 3}
+						])
+					.end()
+					.value({key: 'durable_writes', value: false})
+				.end(),
 			"CREATE KEYSPACE \"hello\" WITH \"replication\" = {'class': 'NetworkTopologyStrategy', 'DC1': 1, 'DC2': 3} AND \"durable_writes\" = false");
 
-		});
-	});
-
-	describe('create table', function()
-	{
-		it('should accept this', function()
-		{
-			var tbl = statement.CreateTable(
-			{
-				table:
-				{
-					table: 'this'
-				},
-
-				primaryKey:
-				{
-					partitionKey: ['id'],
-					clusterKeys: ['created']
-				},
-
-//				columns:
-//				[
-//					{name: 'id', type: ['list', 'uuid']},
-//					{name: 'id2', type: ['map', 'uuid', 'text']}
-//				],
-//
-				options:
-				[
-					"COMPACT STORAGE",
-					{key: 'compaction', value: {key: 'class', value: 'LeveledCompactionStrategy'}}
-				]
-			});
-
-			console.log(tbl.toString());
 		});
 	});
 });
